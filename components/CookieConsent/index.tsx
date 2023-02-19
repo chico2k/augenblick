b07928c38c28b1bded60194a2f21fbca97a4a922
Mario Galla <mariogalla@Marios-MacBook-Pro.fritz.box>
2023-02-19 19:14:52 +0100

    fixed cookie consent on initial load

--- components/CookieConsent/index.tsx
+++ components/CookieConsent/index.tsx
@@ -13,14 +13,17 @@ export const useConsent = () => {
   };
 
   const [consent, setConsent] = useState(defaultConsent);
+  const [hasLoaded, setHasLoaded] = useState(false);
 
   useEffect(() => {
     if (hasCookie("localConsent")) {
       const consent = getCookie("localConsent") as string;
+      setHasLoaded(true);
       return setConsent(JSON.parse(consent));
     }
 
     setCookie("localConsent", defaultConsent, { maxAge: 60 * 60 * 24 * 365 });
+    setHasLoaded(true);
     return setConsent(defaultConsent);
   }, [defaultConsent]);
 
@@ -52,12 +55,14 @@ export const useConsent = () => {
     setConsent(defaultConsent);
     setCookie("localConsent", defaultConsent, { maxAge: 60 * 60 * 24 * 365 });
   };
-  return { consent, acceptCookie, denyCookie, closeCookie };
+  return { consent, acceptCookie, denyCookie, closeCookie, hasLoaded };
 };
 
 function Consent() {
-  const { consent, acceptCookie, denyCookie, closeCookie } = useConsent();
+  const { consent, acceptCookie, denyCookie, closeCookie, hasLoaded } =
+    useConsent();
 
+  if (!hasLoaded) return;
   return (
     <section
       className={`fixed bottom-0 left-0 w-full py-2 md:py-4 ${
@@ -66,13 +71,13 @@ function Consent() {
     >
       <div className="flex flex-col items-start px-5 py-3 space-y-2 bg-gray-200 md:flex-row md:space-y-0 md:items-stretch md:space-x-2">
         <div className="flex items-center flex-grow text-gray-900">
-          <p className="text-sm ">
+          <p className="text-xs mb-2 md:text-sm ">
             Diese Website nutzt Dienste, die Cookies verwenden, um ein besseres
             Erlebnis zu bieten und den Datenverkehr zu analysieren. Mehr über
             die von uns genutzten Dienste erfahren Sie in unserer{" "}
             <Link
               href="/datenschutz"
-              className="text-sm underline hover:text-lightAccent"
+              className="underline hover:text-lightAccent"
             >
               Datenschutzrichtlinie
             </Link>
@@ -82,14 +87,14 @@ function Consent() {
           <button
             type="button"
             onClick={denyCookie}
-            className="mr-4 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="mr-4 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xs md:text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             Ablehnen
           </button>
           <button
             type="button"
             onClick={acceptCookie}
-            className="inline-flex items-center rounded-md border border-transparent bg-fuchsia-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-fuchsia-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="inline-flex items-center rounded-md border border-transparent bg-fuchsia-600 px-4 py-2 text-xs md:text-sm font-medium text-white shadow-sm hover:bg-fuchsia-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             Zustimmen
           </button>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,22 +15,22 @@ const HeroSection: React.FunctionComponent = () => {
         className="object-cover absolute inset-0 h-screen"
       />
       <div className="w-full max-w-7xl mx-auto container z-40">
-        <div className="relative pl-5  text-fuchsia-500 text-5xl tracking-tight z-10 xs:text-6xl sm:tracking-normal sm:text-7xl sm:comtainer md:text-9xl">
+        <div className="relative pl-5  text-fuchsia-500 text-5xl tracking-tight z-10  sm:tracking-normal sm:text-7xl sm:comtainer md:text-9xl">
           Dein
         </div>
-        <h1 className="pl-5 text-fuchsia-500 text-5xl tracking-tight z-20 xs:text-6xl sm:tracking-normal sm:text-7xl md:text-9xl">
+        <h1 className="pl-5 text-fuchsia-500 text-5xl tracking-tight z-20  sm:tracking-normal sm:text-7xl md:text-9xl">
           Augenblick
         </h1>
 
-        <div className="mt-3 text-lg pl-5 sm:text-2xl lg:text-2xl  text-gray-600 tracking-wider        ">
+        <div className="mt-3 text-lg pl-5 sm:text-2xl lg:text-2xl  text-gray-600  ">
           Wimpernverlängerung im Chiemgau
         </div>
-        <div className="ml-5 mt-24 space-x-2 flex lg:text-xl lg:space-x-6">
+        <div className="ml-5 mt-8 md:mt-24 space-x-2 flex lg:text-xl lg:space-x-6">
           <button
             type="button"
             id="click-buchung"
             className="click-buchung inline-flex items-center rounded-md border border-transparent cursor-pointer 
-            bg-gradient-to-tl from-fuchsia-500 via-fuchsia-600 to-fuchsia-700 px-6 py-2 text-2xl font-medium text-white shadow-sm
+            bg-gradient-to-tl from-fuchsia-500 via-fuchsia-600 to-fuchsia-700 px-4 md:px-6 py-2 text-xl md:text-2xl font-medium text-white shadow-sm
             focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
             hover:bg-gradient-to-l hover:from-fuchsia-900 hbover:via-fuchsia-800 hover:to-fuchsia-900  transition-all duration-300 ease-in-out"
           >

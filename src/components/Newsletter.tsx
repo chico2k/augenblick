8f88365c210bfc36a31b95c4dc39c78875fb89d9
Mario <galla@mutably.io>
2023-03-03 22:26:10 +0100

    renamed email in newsletter

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -56,8 +56,8 @@ export default function Newsletter() {
               </label>
               <input
                 {...register("email")}
-                id="email-newsletter"
-                name="email-newsletter"
+                id="email"
+                name="email"
                 type="email"
                 autoComplete="email"
                 required

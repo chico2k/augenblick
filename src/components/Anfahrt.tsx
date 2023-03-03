b0a5ec3b422d9e75d23ca4b2c04e2eebefaa60e1
Mario <galla@mutably.io>
2023-03-03 22:00:47 +0100

    correct title in anfahrt

--- src/components/Anfahrt.tsx
+++ src/components/Anfahrt.tsx
@@ -11,7 +11,7 @@ const AnfahrtSection = () => {
         </div>
         <div className="h-screen w-full text-xs">
           <iframe
-            tile="Anfahrt Augenblick Chiemgau"
+            title="Anfahrt Augenblick Chiemgau"
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10690.83700606855!2d12.6016273!3d47.9420075!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd66571c4270f68c7!2sAugenblick%20Chiemgau!5e0!3m2!1sen!2sde!4v1640699367324!5m2!1sen!2sde"
             width="100%"
             height="100%"

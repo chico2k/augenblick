36bf9d1441904f7ce231ff0310a9e1583120a6dd
Mario <galla@mutably.io>
2023-03-19 16:25:27 +0100

    sitemap v1

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://www.augenblick-chiemgau.com",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
